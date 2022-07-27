72353093e0f822cff939532dd996c2c72662ef7c
Pavel Demin <deminps.2008@gmail.com>
2022-07-27 17:22:07 +0500

    Добавление класса Auth

---
+++ src/utils/auth.js
@@ -0,0 +1,54 @@
+class Auth {
+  constructor(data) {
+    this._url = data.url;
+    this._headers = data.headers;
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+    return res.json();
+  }
+
+  register(data) {
+    return fetch(`${this._url}/signup`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        password: data.password,
+        email: data.email
+      })
+    }).then(this._getResponseData)
+  }
+
+  authorize(data) {
+    return fetch(`${this._url}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        password: data.password,
+        email: data.email
+      })
+    }).then(this._getResponseData);
+  }
+
+  checkToken(token) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: {
+        ...this._headers,
+        Authorization: `Bearer ${token}`
+      }
+    }).then(this._getResponseData)
+  }
+}
+
+const auth = new Auth({
+  baseUrl: 'https://auth.nomoreparties.co',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default auth;
